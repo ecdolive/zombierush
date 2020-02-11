7c78a2a6f2523925f7cb1f61eca793acbda65d5e
Eric Dolive <ecdolive@gmail.com>
2020-02-10 21:57:47 -0500

    - cleanup code

--- init.js
+++ init.js
@@ -3,12 +3,6 @@ $(function(){
   makePlayer();
   makeZombie();
 
-  //game key codes
-  // var KEYLEFT = 37;
-  // var KEYUP = 38;
-  // var KEYRIGHT = 39;
-  // var KEYDOWN = 40;
-
   var map = {};
   onkeydown = onkeyup = function(e){
     e = e || event; // deal with IE
